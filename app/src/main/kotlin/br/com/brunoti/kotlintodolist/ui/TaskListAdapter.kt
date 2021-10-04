1fa079607813f91e2bf8c21efc830d32f41268d3
Bruno Rocha <bruno.roch@gmail.com>
2021-10-04 03:32:08 -0300

    code review, fix edit tasks functions, fix Date and Time Picker

--- app/src/main/kotlin/br/com/brunoti/kotlintodolist/ui/TaskListAdapter.kt
+++ app/src/main/kotlin/br/com/brunoti/kotlintodolist/ui/TaskListAdapter.kt
@@ -12,48 +12,51 @@ import br.com.brunoti.kotlintodolist.model.Task
 
 class TaskListAdapter : ListAdapter<Task, TaskListAdapter.TaskViewHolder>(DiffCallback()) {
 
-    var listenerEdit : (Task) -> Unit = {}
-    var listenerDelete : (Task) -> Unit = {}
+	var listenerEdit: (Task) -> Unit = {}
+	var listenerDelete: (Task) -> Unit = {}
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = ItemTaskBinding.inflate(inflater, parent, false)
-        return TaskViewHolder(binding)
-    }
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+		val inflater = LayoutInflater.from(parent.context)
+		val binding = ItemTaskBinding.inflate(inflater, parent, false)
 
-    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        holder.bind(getItem(position))
-    }
+		return TaskViewHolder(binding)
+	}
 
-    inner class TaskViewHolder(
-        private val binding: ItemTaskBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
+	override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+		holder.bind(getItem(position))
+	}
 
-        fun bind(item: Task) {
-            binding.tvTitle.text = item.title
-            binding.tvDate.text = "${item.date} ${item.hour}"
-            binding.ivMore.setOnClickListener {
-                showPopup(item)
-            }
-        }
+	inner class TaskViewHolder(
+		private val binding: ItemTaskBinding
+	) : RecyclerView.ViewHolder(binding.root) {
 
-        private fun showPopup(item: Task) {
-            val ivMore = binding.ivMore
-            val popupMenu = PopupMenu(ivMore.context, ivMore)
-            popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
-            popupMenu.setOnMenuItemClickListener {
-                when (it.itemId) {
-                    R.id.action_edit -> listenerEdit(item)
-                    R.id.action_delete -> listenerDelete(item)
-                }
-                return@setOnMenuItemClickListener true
-            }
-            popupMenu.show()
-        }
-    }
+		fun bind(item: Task) {
+			binding.tvTitle.text = item.title
+			binding.tvDate.text = "${item.date} ${item.hour}"
+			binding.ivMore.setOnClickListener {
+				showPopup(item)
+			}
+		}
+
+		private fun showPopup(item: Task) {
+			val ivMore = binding.ivMore
+			val popupMenu = PopupMenu(ivMore.context, ivMore)
+
+			popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+			popupMenu.setOnMenuItemClickListener {
+				when (it.itemId) {
+					R.id.action_edit -> listenerEdit(item)
+					R.id.action_delete -> listenerDelete(item)
+				}
+
+				return@setOnMenuItemClickListener true
+			}
+			popupMenu.show()
+		}
+	}
 }
 
 class DiffCallback : DiffUtil.ItemCallback<Task>() {
-    override fun areItemsTheSame(oldItem: Task, newItem: Task) = oldItem == newItem
-    override fun areContentsTheSame(oldItem: Task, newItem: Task) = oldItem.id == newItem.id
+	override fun areItemsTheSame(oldItem: Task, newItem: Task) = oldItem == newItem
+	override fun areContentsTheSame(oldItem: Task, newItem: Task) = oldItem.id == newItem.id
 }
